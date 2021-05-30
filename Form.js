e594bb63690d9aa62c90513987fc0f83f89749eb
Raul Dionisio <rauldionisiosh@gmail.com>
2021-05-30 17:34:34 -0300

    Segunda versão do conversor

--- Form.js
+++ Form.js
@@ -1,29 +1,80 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import Picker from 'react-native-picker-select';
+import {Picker} from '@react-native-picker/picker';
+import Coins from './coins';
+
 
 function Form() {
 
     const [valor, setvalor] = useState("")
+    const [origem, setOrigem] = useState("")
+    const [destino, setDestino] = useState("")
     
-    function handleButtonEuroPress(){
-        let total = valor * 6.78;
-       alert( "Valor convertido: " + total);
+    async function handleButtonEuroPress(){
+        try{
+            let response = await fetch(`https://economia.awesomeapi.com.br/last/${origem}-${destino}`)
+            let responseJson = await response.json();
+            let name = `${origem}${destino}`
+            let compra = responseJson[name].bid;
+            let venda = responseJson[name].ask;
+            let totalCompra = valor * compra;
+            let totalvenda = valor * venda;
+            alert(`Valor para Compra: ${formataValor(totalCompra)} \n Valor para venda: ${formataValor(totalvenda)}`);
+        }catch (error) {
+            console.error(error);
+            alert(`Por favor tente novamente`)
+        }
     }
 
-    function handleButtonDolarPress(){
-        let total = valor * 5.75;
-       alert( "Valor convertido: " + total);
+    function formataValor(valor) {
+     return `$ ${valor.toFixed(2)}`
     }
+
+    const placeholderOrigem = {
+        label: 'De:',
+        value: null,
+        color: 'Black',
+      };
+
+      const placeholderDestino = {
+        label: 'Para:',
+        value: null,
+        color: 'black',
+      };
+
+      const pick =  
+        Coins.map(({label, value, index}) => {
+            return(
+                <Picker.Item label={label} value={value} key={value} />
+            )
+        })
 
      return(
         <View style={styles.inputContainer}>
           <TextInput style={styles.input} onChangeText={text => setvalor(text)} placeholder="Digite o valor" keyboardType={'numeric'} />
-          <TouchableOpacity style={styles.button} onPress={handleButtonDolarPress}>
-            <Text style={styles.buttonText}>Converter para Dolar</Text>
-          </TouchableOpacity>
+        
+        <Picker
+            selectedValue={origem}
+            placeholder={placeholderOrigem}
+            onValueChange={(value) =>{
+                setOrigem(value)
+                console.log(value)
+            }
+            }>
+                <Picker.Item label="De: " value='' key='origem' />
+                {pick}
+        </Picker>
+        <Picker
+            selectedValue={destino}
+            onValueChange={(value) =>
+                setDestino(value)
+            }>
+                <Picker.Item label="Para: " value='' key='destino' />
+                {pick}
+        </Picker>
+
           <TouchableOpacity style={styles.button} onPress={handleButtonEuroPress}>
-            <Text style={styles.buttonText}>Converter para Euro</Text>
+            <Text style={styles.buttonText}>Converter</Text>
          </TouchableOpacity>
          </View>
      );
@@ -59,7 +110,11 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#fff',
         fontWeight: 'bold',
+    },
+    select: {
+        backgroundColor: 'rgb(60, 179, 113)',
     }
+
 });
 
 export default Form;
---
+++ coins.js
@@ -0,0 +1,48 @@
+const Coins = [
+        {
+        label: 'Real Brasileiro',
+        value: 'BRL',
+      },
+        {
+        label: 'Dólar Americano',
+        value: 'USD',
+      },
+      {
+        label: 'Dólar Canadense',
+        value: 'CAD',
+      },
+      {
+        label: 'EURO',
+        value: 'EUR',
+      },
+      {
+        label: 'Libra Esterlina',
+        value: 'GBP',
+      },
+      {
+        label: 'Peso Argentino',
+        value: 'ARS',
+      },
+      {
+        label: 'Iene Japonês',
+        value: 'JPY',
+      },
+      {
+        label: 'Franco Suíço',
+        value: 'CHF',
+      },
+      {
+        label: 'Dólar Australiano',
+        value: 'AUD',
+      },
+      {
+        label: 'Yuan Chinês',
+        value: 'CNY',
+      },
+      {
+        label: 'Novo Shekel Israelense',
+        value: 'ILS',
+      }
+    ]
+
+export default Coins;
